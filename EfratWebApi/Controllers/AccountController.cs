bf0c3a6ec0f19b4aa776387d1a9f0d4e92bf5dbe
Raouf <RaoufManzoor@yahoo.com>
2018-03-26 23:07:49 +0500

    changes are added
    changes Added

---
+++ EfratWebApi/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace EfratWebApi.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*", SupportsCredentials = true)]
+    public class AccountController : ApiController
+    {
+        // GET: Account
+        [HttpPost]
+        public dynamic RegisterUser()
+        {
+             return new List<string>() { "Raouf Manzoor", "Manzoor Hussain Abbasi", "Punjab University" };
+        }
+    }
+}
